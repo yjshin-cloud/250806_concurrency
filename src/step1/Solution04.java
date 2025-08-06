1f06459ce45b03b9781307e57378e30a897b0c33
yjshin-cloud <yjshin.cloud@gmail.com>
2025-08-06 11:07:10 +0900

    parallel 예제 코드

--- src/step1/Solution04.java
+++ src/step1/Solution04.java
@@ -1,7 +1,27 @@
 package step1;
 
+import java.util.List;
+
 public class Solution04 {
     public static void main(String[] args) {
-
+        List<String> textList = List.of("고양이", "호랑이", "하마", "강아지");
+//        textList.stream()
+//                .forEach((text) -> {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    System.out.println(text);
+//                });
+        textList.stream().parallel()
+                .forEach((text) -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(text);
+                });
     }
 }
